ce2dffe739a54f75b208a4012a426933119bb5ee
castellanos94 <aca810@gmail>
2022-02-12 01:53:57 -0600

    Basic Auth

--- src/main/java/com/castellanos94/fuzzylogic/api/controller/QueryController.java
+++ src/main/java/com/castellanos94/fuzzylogic/api/controller/QueryController.java
@@ -6,8 +6,11 @@ import com.castellanos94.fuzzylogic.api.db.FileUtils;
 import com.castellanos94.fuzzylogic.api.model.ResponseModel;
 import com.castellanos94.fuzzylogic.api.model.impl.DiscoveryQuery;
 import com.castellanos94.fuzzylogic.api.model.impl.EvaluationQuery;
-import com.castellanos94.fuzzylogic.api.model.impl.LinguisticState;
 import com.castellanos94.fuzzylogic.api.service.AsynchronousService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
@@ -24,21 +27,28 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
-@CrossOrigin(origins = "http://localhost:8081")
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@SecurityScheme(
+        name = "ApiKey", // can be set to anything
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer"
+)
+
 public class QueryController {
     @Autowired
     EurekaTaskRepository eurekaTaskRepository;
     @Autowired
     AsynchronousService service;
 
-
+    @Operation(security = {@SecurityRequirement(name = "ApiKey")})
     @RequestMapping(value = "query/{id}", method = RequestMethod.GET, produces = {"application/json"})
     public ResponseEntity<EurekaTask> getQuery(@PathVariable String id) {
         Optional<EurekaTask> optionalEurekaTask = eurekaTaskRepository.findById(id);
         return optionalEurekaTask.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NO_CONTENT).build());
     }
 
+    @Operation(security = {@SecurityRequirement(name = "ApiKey")})
     @RequestMapping(value = "query/status/{id}", method = RequestMethod.GET, produces = {"application/json"})
     public ResponseEntity<ResponseModel> getQueryStatus(@PathVariable String id) {
         Optional<EurekaTask> optionalEurekaTask = eurekaTaskRepository.findById(id);
@@ -49,6 +59,7 @@ public class QueryController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(security = {@SecurityRequirement(name = "ApiKey")})
     @RequestMapping(value = "evaluation", method = RequestMethod.GET, produces = {"application/json"})
     public ResponseEntity<Map<String, Object>> getEvaluations(@RequestParam(defaultValue = "0") int page,
                                                               @RequestParam(defaultValue = "3") int size) {
@@ -60,19 +71,20 @@ public class QueryController {
         response.put("queries", queries);
         response.put("currentPage", page);
 
-        long total = eurekaTaskRepository.findAll().stream().filter(q -> q.getQuery() instanceof EvaluationQuery && !(q.getQuery() instanceof DiscoveryQuery)).map(q -> ((EvaluationQuery) q.getQuery())).count() ;
+        long total = eurekaTaskRepository.findAll().stream().filter(q -> q.getQuery() instanceof EvaluationQuery && !(q.getQuery() instanceof DiscoveryQuery)).map(q -> ((EvaluationQuery) q.getQuery())).count();
         response.put("totalItems", total);
-        long totalP = (long) Math.ceil(total/((double)size));
+        long totalP = (long) Math.ceil(total / ((double) size));
         response.put("totalPages", (totalP < 1) ? 1 : totalP);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Operation(security = {@SecurityRequirement(name = "ApiKey")})
     @RequestMapping(value = "evaluation", method = RequestMethod.POST, consumes = {"application/json"}, produces = {"application/json"})
     @Validated
     public ResponseEntity<ResponseModel> upload(@RequestBody @Valid EvaluationQuery evaluationQuery) {
         StringBuilder msgBuilder = new StringBuilder();
-        evaluationQuery.getStates().stream().filter(s -> s.getF() == null || (s.getF()!=null && !s.getF().isValid()))
+        evaluationQuery.getStates().stream().filter(s -> s.getF() == null || (s.getF() != null && !s.getF().isValid()))
                 .forEachOrdered(linguisticState -> msgBuilder.append(linguisticState.getLabel()).append(", "));
 
         ResponseModel responseModel = new ResponseModel();
@@ -89,6 +101,7 @@ public class QueryController {
         return ResponseEntity.ok(responseModel.setStatus(save.getStatus()).setId(save.getId()));
     }
 
+    @Operation(security = {@SecurityRequirement(name = "ApiKey")})
     @RequestMapping(value = "discovery", method = RequestMethod.POST, consumes = {"application/json"}, produces = {"application/json"})
     public ResponseEntity<ResponseModel> uploadDiscovery(@RequestBody @Valid DiscoveryQuery discoveryQuery) {
         ResponseModel responseModel = new ResponseModel();
@@ -116,6 +129,7 @@ public class QueryController {
         return ResponseEntity.ok(responseModel.setStatus(save.getStatus()).setId(save.getId()));
     }
 
+    @Operation(security = {@SecurityRequirement(name = "ApiKey")})
     @RequestMapping(value = "discovery", method = RequestMethod.GET, produces = {"application/json"})
     public ResponseEntity<Map<String, Object>> getDiscoveries(@RequestParam(defaultValue = "0") int page,
                                                               @RequestParam(defaultValue = "3") int size) {
@@ -127,15 +141,16 @@ public class QueryController {
         response.put("queries", queries);
         response.put("currentPage", page);
         response.put("totalItems", queries.size());
-        long total = eurekaTaskRepository.findAll().stream().filter(q -> q.getQuery() instanceof DiscoveryQuery).map(q -> ((DiscoveryQuery) q.getQuery())).count() ;
+        long total = eurekaTaskRepository.findAll().stream().filter(q -> q.getQuery() instanceof DiscoveryQuery).map(q -> ((DiscoveryQuery) q.getQuery())).count();
         response.put("totalItems", total);
-        long totalP = (long) Math.ceil(total/((double)size));
+        long totalP = (long) Math.ceil(total / ((double) size));
 
         response.put("totalPages", (totalP < 1) ? 1 : totalP);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Operation(security = {@SecurityRequirement(name = "ApiKey")})
     @RequestMapping(value = "result/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     public ResponseEntity dowloadResult(@PathVariable String id) throws IOException {
         Optional<EurekaTask> task = eurekaTaskRepository.findById(id);
@@ -153,6 +168,7 @@ public class QueryController {
         return ResponseEntity.badRequest().build();
     }
 
+    @Operation(security = {@SecurityRequirement(name = "ApiKey")})
     @RequestMapping(value = "dataset/{id}", method = RequestMethod.POST, consumes = {"multipart/form-data"}, produces = {"application/json"})
     public ResponseEntity<ResponseModel> uploadFile(@PathVariable String
                                                             id, @RequestParam("file") @Valid @NotNull @NotBlank MultipartFile file) throws IOException {
@@ -178,6 +194,8 @@ public class QueryController {
             return ResponseEntity.badRequest().body(new ResponseModel().setMsg("Id not found"));
         }
     }
+
+    @Operation(security = {@SecurityRequirement(name = "ApiKey")})
     @RequestMapping(value = "dataset/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     public ResponseEntity downloadDataset(@PathVariable String id) throws IOException {
         Optional<EurekaTask> task = eurekaTaskRepository.findById(id);
