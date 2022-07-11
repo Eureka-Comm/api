781833ad7e94a0154c5d701ef179bcbe812935bd
castellanos94 <aca810@gmail>
2022-07-11 16:15:11 -0500

    Execution save result in EurekaTask

--- src/main/java/com/castellanos94/fuzzylogic/api/service/TaskThread.java
+++ src/main/java/com/castellanos94/fuzzylogic/api/service/TaskThread.java
@@ -7,15 +7,14 @@ import com.castellanos94.fuzzylogic.api.model.impl.DiscoveryQuery;
 import com.castellanos94.fuzzylogic.api.model.impl.EvaluationQuery;
 import com.castellanos94.fuzzylogicgp.algorithm.EvaluatePredicate;
 import com.castellanos94.fuzzylogicgp.algorithm.KDFLC;
-import com.castellanos94.fuzzylogicgp.core.Node;
+import com.castellanos94.fuzzylogicgp.core.DiscoveryResult;
+import com.castellanos94.fuzzylogicgp.core.EvaluationResult;
 import com.castellanos94.fuzzylogicgp.core.NodeTree;
-import com.castellanos94.fuzzylogicgp.core.NodeType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import tech.tablesaw.api.StringColumn;
 import tech.tablesaw.api.Table;
 
 import java.io.File;
@@ -40,7 +39,7 @@ public class TaskThread implements Runnable {
     @Override
     public void run() {
         LOGGER.info("Start task:" + task.getId());
-        repository.save(task.setStatus(EurekaTask.Status.Running));
+        repository.save(task.setStatus(EurekaTask.Status.Running).setStart(new Date()));
         NodeTree predicateTree = null;
         try {
             predicateTree = task.getQuery().getPredicateTree();
@@ -50,7 +49,7 @@ public class TaskThread implements Runnable {
             task.setStatus(EurekaTask.Status.Failed);
         }
         if (predicateTree != null) {
-            com.castellanos94.fuzzylogicgp.logic.Logic _logic = null;
+            com.castellanos94.fuzzylogicgp.logic.Logic _logic ;
             File output;
             Table table = null;
             try {
@@ -62,68 +61,24 @@ public class TaskThread implements Runnable {
             }
             if (table != null) {
                 if (task.getQuery() instanceof EvaluationQuery && !(task.getQuery() instanceof DiscoveryQuery)) {
-                    _logic = ((EvaluationQuery) task.getQuery()).getLogic().toInternalObject().build();
+                    _logic = task.getQuery().getLogic().toInternalObject().build();
                     LOGGER.error("Logic {}", _logic);
                     try {
                         EvaluatePredicate evaluatePredicate = new EvaluatePredicate(_logic, table);
                         evaluatePredicate.evaluate(predicateTree);
-                       /* ArrayList<Node> operators = NodeTree.getNodesByType(predicateTree, NodeType.AND);
-                        for (Node node : operators) {
-                            if (node instanceof NodeTree && !node.equals(predicateTree)) {
-                                EvaluatePredicate ep = new EvaluatePredicate(_logic, table);
-                                double evaluate = ep.evaluate((NodeTree) node);
-                                ((NodeTree) node).setFitness(evaluate);
-                            }
+                        ArrayList<NodeTree> operators = NodeTree.getNodesByType(predicateTree, NodeTree.class);
+                        EvaluationResult evaluationResult = (EvaluationResult) evaluatePredicate.getResult();
+                        for (NodeTree op : operators) {
+                            evaluatePredicate = new EvaluatePredicate(_logic, table);
+                            op.setFitness(evaluatePredicate.evaluate(op.copy()));
                         }
-                        operators = NodeTree.getNodesByType(predicateTree, NodeType.OR);
-                        for (Node node : operators) {
-                            if (node instanceof NodeTree && !node.equals(predicateTree)) {
-                                EvaluatePredicate ep = new EvaluatePredicate(_logic, table);
-                                double evaluate = ep.evaluate((NodeTree) node);
-                                ((NodeTree) node).setFitness(evaluate);
-                            }
-                        }
-                        operators = NodeTree.getNodesByType(predicateTree, NodeType.NOT);
-                        for (Node node : operators) {
-                            if (node instanceof NodeTree && !node.equals(predicateTree)) {
-                                EvaluatePredicate ep = new EvaluatePredicate(_logic, table);
-                                double evaluate = ep.evaluate((NodeTree) node);
-                                ((NodeTree) node).setFitness(evaluate);
-                            }
-                        }
-                        operators = NodeTree.getNodesByType(predicateTree, NodeType.IMP);
-                        for (Node node : operators) {
-                            if (node instanceof NodeTree && !node.equals(predicateTree)) {
-                                EvaluatePredicate ep = new EvaluatePredicate(_logic, table);
-                                double evaluate = ep.evaluate((NodeTree) node);
-                                ((NodeTree) node).setFitness(evaluate);
-                            }
-                        }
-                        operators = NodeTree.getNodesByType(predicateTree, NodeType.EQV);
-                        for (Node node : operators) {
-                            if (node instanceof NodeTree && !node.equals(predicateTree)) {
-                                EvaluatePredicate ep = new EvaluatePredicate(_logic, table);
-                                double evaluate = ep.evaluate((NodeTree) node);
-                                ((NodeTree) node).setFitness(evaluate);
-                            }
-                        }*/
-                        output = FileUtils.GET_OUTPUT_FILE(task.getId());
-
-                        evaluatePredicate.exportToCsv(output.getAbsolutePath());
-                        Table result = Table.read().csv(output);
-                        StringColumn predicateColumn = StringColumn.create("data");
-                        predicateColumn.append(evaluatePredicate.getPredicate().toJson());
-                        for (int i = 1; i < result.rowCount(); ++i) {
-                            predicateColumn.append("");
-                        }
-                        result.addColumns(predicateColumn);
-                        result.write().csv(output);
+                        evaluationResult.setPredicate(predicateTree);
+                        task.setResultTask(evaluationResult);
                         task.setMsg("Done " + new Date());
                         task.setStatus(EurekaTask.Status.Done);
                     } catch (Exception e) {
                         LOGGER.error("Evaluation algorithm (or saving dataset)", e);
                         task.setMsg("Failed " + new Date() + " " + e.getMessage());
-//			e.printStackTrace();
                         task.setStatus(EurekaTask.Status.Failed);
                     }
                 } else if (task.getQuery() instanceof DiscoveryQuery) {
@@ -136,16 +91,10 @@ public class TaskThread implements Runnable {
                                 discoveryQuery.getAdjPopulationSize(), discoveryQuery.getAdjNumberOfIterations(), discoveryQuery.getAdjMinimumTruthValue(), table, discoveryQuery.getMaxTime());
 
                         algorithm.execute(predicateTree);
-                        output = FileUtils.GET_OUTPUT_FILE(task.getId());
-                        try {
-                            algorithm.exportResult(output);
-                            task.setMsg("Done " + new Date());
-                            task.setStatus(EurekaTask.Status.Done);
-                        } catch (Exception e) {
-                            LOGGER.error("Error trying export", e);
-                            task.setMsg("Error during export of results" + new Date() + " " + e.getMessage());
-                            task.setStatus(EurekaTask.Status.Failed);
-                        }
+                        DiscoveryResult discoveryResult = (DiscoveryResult) algorithm.getResult();
+                        task.setResultTask(discoveryResult);
+                        task.setMsg("Done " + new Date());
+                        task.setStatus(EurekaTask.Status.Done);
                     } catch (Exception e) {
                         LOGGER.error("Discovery algorithm", e);
                         task.setMsg("Failed " + new Date() + " " + e.getMessage());
@@ -158,7 +107,7 @@ public class TaskThread implements Runnable {
             task.setMsg("Invalid predicate " + new Date());
             task.setStatus(EurekaTask.Status.Failed);
         }
-        repository.save(task);
+        repository.save(task.setEnd(new Date()));
         if (!task.getQuery().isPublic()) {
             FileUtils.DELETE_DATASET(task.getId());
         }
